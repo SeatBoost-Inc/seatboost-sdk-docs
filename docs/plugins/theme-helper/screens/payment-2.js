9aa4fe56b569b92d59b52e25d99b6eda70c7fe02
Alessandro Jatoba <alessandro@quimbik.com>
2024-02-11 17:06:45 -0300

    terms and review styles

--- docs/plugins/theme-helper/screens/payment-2.js
+++ docs/plugins/theme-helper/screens/payment-2.js
@@ -1,5 +1,8 @@
 import { paymentCoords  } from './payment.js';
 
 export var payment2Coords = Object.assign({}, paymentCoords, {
-
+	'tip-icon': ['11,257,45,296', '11,302,45,341', '11,347,45,386'],
+	'tip': ['45,257,258,296', '45,297,258,346', '45,347,258,386'],
+	'p-text': ['43,193,233,242', '5,460,271,508'],
+	'submit-button': ['8,533,269,568']
 }); 
--- docs/plugins/theme-helper/screens/payment-3.js
+++ docs/plugins/theme-helper/screens/payment-3.js
@@ -1,5 +1,7 @@
 import { paymentCoords  } from './payment.js';
 
 export var payment3Coords = Object.assign({}, paymentCoords, {
-
+	'section-bar':  ['0,189,277,208', '0,303,277,323'],
+	'review-msg': ['15,489,261,532'],
+	'submit-button': ['12,537,265,572']
 }); 
