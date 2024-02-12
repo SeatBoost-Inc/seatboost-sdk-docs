0de11da0bb6f50c44e6a2c044291e0bb5958acba
Alessandro Jatoba <alessandro@quimbik.com>
2024-02-12 08:11:27 -0300

    payment screens

--- docs/plugins/theme-helper/screens/payment-4.js
+++ docs/plugins/theme-helper/screens/payment-4.js
@@ -1,5 +1,12 @@
 import { paymentCoords  } from './payment.js';
 
 export var payment4Coords = Object.assign({}, paymentCoords, {
-
+	'section-bar':  ['0,189,277,208'],
+	'apple-pay-btn': ['14,221,264,258'],
+	'pay-using-label': ['14,258,264,280'],
+	'text-field': ['14,281,264,318', '14,321,264,358'],
+	'radio-button': ['12,360,41,388'], 
+	'payment-label': ['42,360,171,388','15,479,261,532'],
+	'submit-button': ['21,539,256,572'],
+	'form-bg': ['0,156,277,599']
 }); 
