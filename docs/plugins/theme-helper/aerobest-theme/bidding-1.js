5eefc94bdf1a2c19343d34a3aa7b97127b2d621f
Alessandro Jatoba <alessandro@quimbik.com>
2024-02-13 15:34:16 -0300

    bidding navigation bar

--- docs/plugins/theme-helper/aerobest-theme/bidding-1.js
+++ docs/plugins/theme-helper/aerobest-theme/bidding-1.js
@@ -1,4 +1,7 @@
 export var bidding1Coords = Object.assign({}, [], {
+	'auction-navbar-icon': ['239,35,273,61'], 
+	'auction-navbar-airline-logo': ['66,35,211,61'], 
+	'auction-navbar': ['0,32,277,64'],
 	'timer': ['87,184,190,230'],
 	'leaderboard-bar': ['0,196,87,217', '191,196,277,217'],
 	'leaderboard-bg': ['0,64,277,197'],
