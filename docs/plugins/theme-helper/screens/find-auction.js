100f8f380d54bb0ec5ec78cf5fbb7588f31a25df
Alessandro Jatoba <alessandro@quimbik.com>
2024-02-13 10:00:19 -0300

    find auctions areas

--- docs/plugins/theme-helper/screens/find-auction.js
+++ docs/plugins/theme-helper/screens/find-auction.js
@@ -1,4 +1,11 @@
 import { preAuctionTopCoords  } from './pre-auction.js';
 
 export var findAuctionCoords = Object.assign({}, preAuctionTopCoords, {
+	'airline-logo': ['40,80,237,120'],
+	'message': ['10,130,267,156'],
+	'help-icon': ['242,153,272,182'],
+	'form-label': ['12,179,265,200', '12,236,265,257', '12,293,265,315'],
+	'text-field': ['12,200,265,236', '12,257,265,294', '12,315,265,352'],
+	'submit-button': ['13,547,265,581'],
+	'screen-bg': ['0,28,277,599']
 }); 
